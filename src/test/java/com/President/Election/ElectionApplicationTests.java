d5b6d12e4023dfcf6e4a204e4d7d351d729f691a
alojine <benaspaliokas@gmail.com>
2023-10-05 12:10:01 +0300

    initial commit

---
+++ src/test/java/com/President/Election/ElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.President.Election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
